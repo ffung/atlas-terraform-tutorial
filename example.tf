8b1ff2a5dce0d9c2d26b85d3b11f8f697c6d4782
Fai Fung <ffung@xebia.com>
2015-06-26 10:21:47 +0200

    add consul cluster

--- example.tf
+++ example.tf
@@ -60,3 +60,11 @@ resource "aws_main_route_table_association" "a" {
     vpc_id = "${aws_vpc.main.id}"
     route_table_id = "${aws_route_table.r.id}"
 }
+
+#--------------------------------------------------------------
+# Consul Cluster
+#--------------------------------------------------------------
+module "consul" {
+    source = "github.com/hashicorp/consul/terraform/aws"
+    servers = "3"
+}
